81d40a4972f1caa1efdce14fc4efabf01afa8afd
Alexey Kostyuchik <alesnikos@gmail.com>
2018-08-10 22:22:49 +0500

    commit

--- lesson_3/js/script.js
+++ lesson_3/js/script.js
@@ -1,6 +1,7 @@
 let budget, 
 	nameStore,
-	time
+	time,
+	price
 
 function start() {
 		budget = prompt('Ваш бюджет на месяц?');
@@ -19,7 +20,8 @@ let mainList = {
 	nameStore: nameStore,
 	shopGoods: [],
 	employers: {},
-	open: true
+	open: true,
+	discount: true
 };
 
 function chooseGoods() {
@@ -36,15 +38,44 @@ function chooseGoods() {
 }
 chooseGoods();
 
-function workTime() {
+function workTime(time) {
 	if (time < 0) {
 		console.log('Такого не может быть!');
 	} else if (time > 8 && time < 20) {
 		console.log('Время работать!');
 	} else if (time < 24) {
-		console.log('Уже слишком поздно')
+		console.log('Уже слишком поздно');
+	} else {
+		console.log('В сутках только 24 часа!');
 	}
 }
+workTime(18);
 
-alert('Ваш бюджет на 1 день: ' + Math.round(mainList.budget / 30) + ' денежных единиц');
+function dayBudget() {
+	alert('Ваш бюджет на 1 день: ' + Math.round(mainList.budget / 30) + ' денежных единиц');
+
+}
+dayBudget();
+
+function makeDiscount(price) {
+	if (mainList.discount == true) {
+		price = price * 0.8;
+	}
+	console.log(price);
+}
+makeDiscount(1000);
+
+function hireEmployees() {
+	for (let i = 1; i <= 4; i++) {
+		let b = prompt('Как зовут сотрудника?');
+
+			if ((typeof(b)) === 'string' && b !== null && b != '' && b.length < 50) {
+			console.log('Все верно!');
+			mainList.employers[i] = b;
+		} else {
+			i = i - 1;
+		}
+	}
+}
+// hireEmployees();
 console.log(mainList);
