00c77e93530282211a184c228db3fc596df5c91f
Alexey Kostyuchik <alesnikos@gmail.com>
2018-08-20 21:59:35 +0500

    commit

--- lesson_7/js/timer.js
+++ lesson_7/js/timer.js
@@ -1,25 +1,6 @@
-let timerId = setTimeout(showTimer, 1000);
+setInterval(showTime, 1000);
 
-
-function formatTime(date) {
-	let HH = date.getHours();
-	if (HH < 10) {
-		HH = '0' + HH;
-	}
-	let mm = date.getMinutes();
-	if (mm < 10) {
-		mm = '0' + mm;
-	}
-	let ss = date.getSeconds();
-	if (ss < 10) {
-		ss = '0' + ss;
-	}
-	return HH + ':' + mm + ':' + ss;
-}
-
-function showTimer() {
+function showTime() {
 	let a = new Date();
-	document.write(formatTime(a) + '<br>');
-	// timerId = setTimeout(showTimer, 1000)
+	document.getElementById('clock').innerHTML = a.toLocaleTimeString();
 }
-
