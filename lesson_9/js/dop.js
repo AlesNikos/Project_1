9a06e550a8289180c9800894f80a10874d89d237
Alexey Kostyuchik <alesnikos@gmail.com>
2018-08-23 21:36:14 +0500

    commit

--- lesson_9/js/dop.js
+++ lesson_9/js/dop.js
@@ -1,7 +1,7 @@
 let age = document.getElementById('age');
 
 age.addEventListener('change', function showUser(surname,name) {
-	this.surname = "Пупкин";
-	this.name = "Василий";
-	alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + this.value);
+	surname = "Пупкин";
+	name = "Василий";
+	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 });
